e6d41dd23e56e6625d1cc5fde8657630e2aa258c
maxgu <gulturyan@gmail.com>
2015-09-30 15:56:40 +0300

    refactor module.config

---
+++ src/T4webAdmin/Controller/ListControllerFactory.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace T4webAdmin\Controller;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+
+class ListControllerFactory implements FactoryInterface
+{
+    public function createService(ServiceLocatorInterface $controllerManager)
+    {
+        /** @var \Zend\ServiceManager\ServiceManager $serviceLocator */
+        $serviceLocator = $controllerManager->getServiceLocator();
+        /** @var \Zend\Mvc\Application $app */
+        $app = $serviceLocator->get('Application');
+        /** @var \Zend\Mvc\Router\Http\RouteMatch $routeMatch */
+        $routeMatch = $app->getMvcEvent()->getRouteMatch();
+
+        $module = $routeMatch->getParam('module');
+        $entity = $routeMatch->getParam('entity');
+        $umodule = ucfirst($module);
+        $uentity = ucfirst($entity);
+
+        $query = $serviceLocator->get('request')->getQuery()->toArray();
+        $inputFilter = new \T4webAdmin\InputFilter\ListInputFilter($umodule, $uentity);
+
+
+        $repository = $serviceLocator->get("$umodule\\$uentity\Repository\DbRepository");
+        $criteriaFactory = $serviceLocator->get("$umodule\\$uentity\Criteria\CriteriaFactory");
+        $finder = new \T4webBase\Domain\Service\BaseFinder($repository, $criteriaFactory);
+
+        // View
+
+        $viewModel = new \T4webAdmin\View\Model\ListViewModel();
+        $viewModel->setTemplate('t4web-admin/list');
+        $viewModel->setVariable('route', 'admin-' . $module . '-' . $entity);
+
+        // FilterView
+        $filterViewModel = new \T4webAdmin\View\Model\ListFilterViewModel($inputFilter);
+        $filterViewModel->setTemplate('t4web-admin/list-filter');
+
+        // PaginatorView
+        $paginatorViewModel = new \T4webAdmin\View\Model\PaginatorViewModel($inputFilter, $finder);
+        $paginatorViewModel->setTemplate('t4web-admin/paginator');
+
+        // TableView
+        $tableViewModel = new \T4webAdmin\View\Model\TableViewModel();
+        $tableViewModel->setTemplate('t4web-admin/list-table');
+        $tableViewModel->setVariables($viewModel->getVariables());
+
+
+        // head
+        $config = $serviceLocator->get('config');
+
+        if (!empty($config['t4web-admin'][$module][$entity]['list']['table']['head'])) {
+            $headConfig = $config['t4web-admin'][$module][$entity]['list']['table']['head'];
+
+            $tableHeadView = new \T4webAdmin\View\Model\TableHeadViewModel();
+            $tableHeadView->setTemplate('t4web-admin/list-table-head');
+            $tableHeadView->setVariables($tableViewModel->getVariables());
+
+            foreach ($headConfig as $head) {
+                $tableHeadColumnView = new \T4webAdmin\View\Model\TableHeadColumnViewModel();
+                $tableHeadColumnView->setTemplate('t4web-admin/list-table-head-column');
+                $tableHeadColumnView->setVariables($tableViewModel->getVariables());
+                $tableHeadColumnView->setValue($head);
+
+                $tableHeadView->addColumnView($tableHeadColumnView);
+            }
+
+            $tableViewModel->setHeadView($tableHeadView);
+        }
+
+        // row
+        if (!empty($config['t4web-admin'][$module][$entity]['list']['table']['row'])) {
+            $rowConfig = $config['t4web-admin'][$module][$entity]['list']['table']['row'];
+
+            $tableRowView = new \T4webAdmin\View\Model\TableRowViewModel();
+            $tableRowView->setTemplate('t4web-admin/list-table-row');
+            $tableRowView->setVariables($tableViewModel->getVariables());
+
+            foreach ($rowConfig as $row) {
+                $tableColumnView = new \T4webAdmin\View\Model\TableColumnViewModel();
+                $tableColumnView->setTemplate('t4web-admin/list-table-row-column');
+                $tableColumnView->setVariables($tableRowView->getVariables());
+                $tableColumnView->setEntityAttribute($row);
+
+                $tableRowView->addColumnView($tableColumnView);
+            }
+
+            $tableViewModel->setRowView($tableRowView);
+        }
+
+
+        $viewModel->addChild($filterViewModel, 'filter');
+        $viewModel->addChild($paginatorViewModel, 'paginator', true);
+        $viewModel->setTableViewModel($tableViewModel);
+
+        $instance = new \T4webAdmin\Controller\ListController(
+            $query,
+            $inputFilter,
+            $finder,
+            $viewModel
+        );
+
+        return $instance;
+    }
+}
