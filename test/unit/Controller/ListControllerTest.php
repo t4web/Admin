6b613a6e6cdec40bff3cc23cfe087c40074f30c5
maxgu <gulturyan@gmail.com>
2015-09-30 15:38:33 +0300

    update ListControllerTest

--- test/unit/Controller/ListControllerTest.php
+++ test/unit/Controller/ListControllerTest.php
@@ -2,15 +2,14 @@
 
 namespace T4webAdmin\UnitTest\Controller\Admin;
 
-use T4webAdmin\Controller\CrudController;
+use T4webAdmin\Controller\ListController;
 use T4webAdmin\View\Model\ListViewModel;
 use T4webBase\Domain\Collection;
 
-class CrudControllerTest extends \PHPUnit_Framework_TestCase {
+class ListControllerTest extends \PHPUnit_Framework_TestCase {
 
     public function testListAction()
     {
-        return $this->markTestIncomplete();
         $query = ['foo' => 'bar'];
         $filteredQuery = $query;
         $collection = new Collection();
@@ -18,6 +17,7 @@ class CrudControllerTest extends \PHPUnit_Framework_TestCase {
         $inputFilterMock = $this->getMockBuilder('T4webBase\InputFilter\Filter')
             ->disableOriginalConstructor()
             ->getMock();
+
         $inputFilterMock->method('filter')
             ->with($query)
             ->will($this->returnValue($filteredQuery));
@@ -25,19 +25,20 @@ class CrudControllerTest extends \PHPUnit_Framework_TestCase {
         $finderMock = $this->getMockBuilder('T4webBase\Domain\Service\BaseFinder')
             ->disableOriginalConstructor()
             ->getMock();
+
         $finderMock->method('findMany')
             ->with($filteredQuery)
             ->will($this->returnValue($collection));
 
         $listViewModel = new ListViewModel();
 
-        $controller = new CrudController();
-        $actualViewModel = $controller->listAction(
+        $controller = new ListController(
             $query,
             $inputFilterMock,
             $finderMock,
             $listViewModel
         );
+        $actualViewModel = $controller->listAction();
 
         $this->assertSame($listViewModel, $actualViewModel);
         $this->assertSame($collection, $actualViewModel->getMainCollection());
