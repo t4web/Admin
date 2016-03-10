22cbfd88ff75b870e05ae937d099cadea6884caf
maxgu <gulturyan@t4web.com.ua>
2016-03-10 11:19:06 +0200

    add checking Sebaks\View and Sebaks\ZendMvcController module  load

--- src/Module.php
+++ src/Module.php
@@ -3,9 +3,31 @@
 namespace T4web\Admin;
 
 use Zend\ModuleManager\Feature\ConfigProviderInterface;
+use Zend\ModuleManager\Feature\BootstrapListenerInterface;
+use Zend\EventManager\EventInterface;
+use Zend\ModuleManager\ModuleManager;
+use Zend\ModuleManager\Exception\MissingDependencyModuleException;
 
-class Module implements ConfigProviderInterface
+class Module implements ConfigProviderInterface, BootstrapListenerInterface
 {
+    public function onBootstrap(EventInterface $e)
+    {
+        /** @var ModuleManager $moduleManager */
+        $moduleManager = $e->getApplication()
+            ->getServiceManager()
+            ->get('modulemanager');
+
+        if (!$moduleManager->getModule('Sebaks\View')) {
+            throw new MissingDependencyModuleException('Module "Sebaks\View" must be enabled in your
+                config/application.config.php. For details see https://github.com/t4web/Admin#post-installation.');
+        }
+
+        if (!$moduleManager->getModule('Sebaks\ZendMvcController')) {
+            throw new MissingDependencyModuleException('Module "Sebaks\ZendMvcController" must be enabled in your
+                config/application.config.php. For details see https://github.com/t4web/Admin#post-installation.');
+        }
+    }
+
     public function getConfig($env = null)
     {
         return include __DIR__ . '/../config/module.config.php';
