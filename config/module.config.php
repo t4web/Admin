89258d89cc5076748109976c47ab3d06bdc6d58e
Sebas <dima@t4web.com.ua>
2016-03-10 18:11:06 +0200

    Remove view-component- from view blocks names

--- config/module.config.php
+++ config/module.config.php
@@ -61,46 +61,46 @@ return [
             't4web-admin-top-panel' => [
                 'template' => 't4web-admin/top-panel',
             ],
-            't4web-admin-view-component-list' => [
+            't4web-admin-list' => [
                 'template' => 't4web-admin/list',
                 'viewModel' => 'T4web\Admin\ViewModel\EntityListViewModel',
                 'variables' => [
                     'title' => 'List of entities',
                 ],
                 'children' => [
-                    'filter' => 't4web-admin-view-component-list-filter',
-                    'table' => 't4web-admin-view-component-list-table',
-                    'paginator' => 't4web-admin-view-component-list-paginator',
+                    'filter' => 't4web-admin-list-filter',
+                    'table' => 't4web-admin-list-table',
+                    'paginator' => 't4web-admin-list-paginator',
                 ],
             ],
-            't4web-admin-view-component-list-filter' => [
+            't4web-admin-list-filter' => [
                 'template' => 't4web-admin/list-filter',
             ],
-            't4web-admin-view-component-list-table' => [
+            't4web-admin-list-table' => [
                 'template' => 't4web-admin/list-table',
                 'children' => [
-                    'table-head' => 't4web-admin-view-component-list-table-head',
-                    'table-row' => 't4web-admin-view-component-list-table-row',
+                    'table-head' => 't4web-admin-list-table-head',
+                    'table-row' => 't4web-admin-list-table-row',
                 ],
             ],
-            't4web-admin-view-component-list-paginator' => [
+            't4web-admin-list-paginator' => [
                 'template' => 't4web-paginator/paginator',
             ],
-            't4web-admin-view-component-list-table-head' => [
+            't4web-admin-list-table-head' => [
                 'template' => 't4web-admin/list-table-head',
                 'children' => [
                 ],
             ],
-            't4web-admin-view-component-list-table-head-column' => [
+            't4web-admin-list-table-head-column' => [
                 'template' => 't4web-admin/list-table-head-column',
             ],
-            't4web-admin-view-component-list-table-row' => [
+            't4web-admin-list-table-row' => [
                 'template' => 't4web-admin/list-table-row',
             ],
-            't4web-admin-view-component-list-table-row-column' => [
+            't4web-admin-list-table-row-column' => [
                 'template' => 't4web-admin/list-table-row-column',
             ],
-            't4web-admin-view-component-read' => [
+            't4web-admin-read' => [
                 'template' => 't4web-admin/entity-read',
                 'viewModel' => 'T4web\Admin\ViewModel\EntityReadViewModel',
                 'variables' => [
@@ -110,14 +110,14 @@ return [
                 ],
                 'children' => [
                     'form' => [
-                        'extend' => 't4web-admin-view-component-form',
+                        'extend' => 't4web-admin-form',
                         'variables' => [
                             'submitText' => 'Save',
                         ],
                     ],
                 ],
             ],
-            't4web-admin-view-component-update' => [
+            't4web-admin-update' => [
                 'template' => 't4web-admin/entity-update',
                 'variables' => [
                     'title' => 'Update entity',
@@ -125,30 +125,30 @@ return [
                     'cancelRoute' => '',
                 ],
                 'children' => [
-                    'form' => 't4web-admin-view-component-form',
+                    'form' => 't4web-admin-form',
                 ],
             ],
-            't4web-admin-view-component-form' => [
+            't4web-admin-form' => [
                 'template' => 't4web-admin/form',
                 'variables' => [
                     'submitText' => 'Create',
                     'cancelText' => 'Cancel',
                 ],
             ],
-            't4web-admin-view-component-form-element-text' => [
+            't4web-admin-form-element-text' => [
                 'template' => 't4web-admin/form-element-text',
             ],
-            't4web-admin-view-component-form-element-select' => [
+            't4web-admin-form-element-select' => [
                 'template' => 't4web-admin/form-element-select',
             ],
 
-            't4web-admin-view-component-create' => [
+            't4web-admin-create' => [
                 'template' => 't4web-admin/entity-create',
                 'variables' => [
                     'title' => 'Create new entity',
                 ],
                 'children' => [
-                    'form' => 't4web-admin-view-component-form'
+                    'form' => 't4web-admin-form'
                 ],
             ],
         ],
